fb471b8a6d6d965d334284ef8594b882bb3f5e81
AlexanderChugunov <108572174+AlexanderChugunov@users.noreply.github.com>
2024-03-05 22:52:36 +0300

    Update cell.h

--- spreadsheet/cell.h
+++ spreadsheet/cell.h
@@ -5,32 +5,38 @@
 
 #include <functional>
 #include <unordered_set>
+#include <optional>
+#include <set>
+#include <map>
 
 class Sheet;
+class Impl;
 
 class Cell : public CellInterface {
 public:
     Cell(Sheet& sheet);
     ~Cell();
 
-    void Set(std::string text);
+    void Set(std::string text, Position pos);
     void Clear();
 
     Value GetValue() const override;
     std::string GetText() const override;
     std::vector<Position> GetReferencedCells() const override;
-
-    bool IsReferenced() const;
+    void GetTextCash();
+    void ReferencedCells();
+    void FindCyclicDependencies(Position pos);
+    void UpdateCache();
 
 private:
     class Impl;
     class EmptyImpl;
     class TextImpl;
     class FormulaImpl;
-
     std::unique_ptr<Impl> impl_;
-
-    // Добавьте поля и методы для связи с таблицей, проверки циклических 
-    // зависимостей, графа зависимостей и т. д.
+    Sheet& sheet_;
+    mutable std::optional<double> cache_;
+    std::string text_cash;
+    bool is_text_cash = false;
 
 };
